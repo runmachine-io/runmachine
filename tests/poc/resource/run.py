fe2674002f4682769c920f95377b4f5ba4ba16fb
Jay Pipes <jaypipes@gmail.com>
2018-09-07 19:51:44 -0400

    start fleshing out resource RDBMS schema poc
    It's important to proof-of-concept a proposed RDBMS schema for the
    resource service, so this gets that process going by adding a schema
    file, a reset DB function, and some apparatus for creating the records
    for various scenarios we will need in the poc.

---
+++ tests/poc/resource/run.py
@@ -0,0 +1,84 @@
+# Loads up the runm-resource database with records that we then use in our PoC
+# scenarios
+
+import argparse
+import os
+import subprocess
+import sys
+
+import sqlalchemy as sa
+
+import resource_models
+
+_LOG_FORMAT = "%(level)s %(message)s"
+_RESOURCE_SCHEMA_FILE = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), 'resource_schema.sql',
+)
+
+
+class RunContext(object):
+    def __init__(self, args):
+        self.args = args
+
+    def status(self, msg):
+        sys.stdout.write(msg + " ... ")
+        sys.stdout.flush()
+
+    def status_ok(self, ):
+        sys.stdout.write("ok\n")
+        sys.stdout.flush()
+
+    def status_fail(self, err):
+        sys.stdout.write("FAIL\n")
+        sys.stdout.flush()
+        sys.stderr.write(" error: %s\n" % err)
+        sys.stderr.flush()
+
+
+def reset_db(ctx):
+    ctx.status("resetting resource PoC database")
+    db_user = os.environ.get('RUNM_TEST_RESOURCE_DB_USER', 'root')
+    db_pass = os.environ.get('RUNM_TEST_RESOURCE_DB_PASS', '')
+    cmd = "mysql -u%s -p%s 2>/dev/null < %s" % (
+        db_user, db_pass, _RESOURCE_SCHEMA_FILE,
+    )
+    subprocess.check_output(cmd, shell=True)
+    ctx.status_ok()
+
+
+def create_resource_classes(ctx):
+    ctx.status("creating resource classes")
+    rc_tbl = resource_models.get_table('resource_classes')
+    sess = resource_models.get_session()
+
+    rcs = [
+        dict(code='VCPU', description='virtual CPU'),
+        dict(code='MEMORY_BYTES', description='Bytes of RAM'),
+        dict(code='BLOCK_STORAGE_BYTES', description='Bytes of block storage'),
+        dict(code='VGPU', description='virtual GPU context'),
+    ]
+
+    for rec in rcs:
+        ins = rc_tbl.insert().values(**rec)
+        sess.execute(ins)
+    sess.commit()
+    ctx.status_ok()
+
+
+def setup_opts(parser):
+    parser.add_argument('--reset', action='store_true',
+                        default=True, help="Reset the database entirely.")
+
+
+def main(ctx):
+    if ctx.args.reset:
+        reset_db(ctx)
+    create_resource_classes(ctx)
+
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser(description='Load up resource database.')
+    setup_opts(p)
+    args = p.parse_args()
+    ctx = RunContext(args)
+    main(ctx)

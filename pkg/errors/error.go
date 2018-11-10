72e9b3c872d7b209fc39fb11deff495b6333ecec
Jay Pipes <jaypipes@gmail.com>
2018-11-10 09:19:32 -0500

    Adds a generic errors package
    When fleshing out the metadata service, I found it would be useful to be
    able to return errors that had a bit more detail in them than the fairly
    sparse gRPC fault codes. So, this commit adds a simple generic errors
    package that will be used to pass more contextual errors back from the
    storage and other layers to the gRPC server layers (which the server
    layer may translate to gRPC-specific errors)

---
+++ pkg/errors/error.go
@@ -0,0 +1,38 @@
+package errors
+
+import (
+	"fmt"
+)
+
+type Error struct {
+	HTTPCode int
+	Code     int
+	Message  string
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("ERROR(%d): %s", e.Code, e.Message)
+}
+
+var (
+	ErrNotFound = &Error{
+		HTTPCode: 404,
+		Code:     404,
+		Message:  "object could not be found.",
+	}
+	ErrDuplicate = &Error{
+		HTTPCode: 409,
+		Code:     409001,
+		Message:  "object already exists.",
+	}
+	ErrMultipleRecords = &Error{
+		HTTPCode: 409,
+		Code:     409002,
+		Message:  "found multiple records when expected to find one.",
+	}
+	ErrGenerationConflict = &Error{
+		HTTPCode: 409,
+		Code:     409003,
+		Message:  "encountered generation conflict.",
+	}
+)

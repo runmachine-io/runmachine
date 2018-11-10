2fb4c3cbfeff296207dcd702e74e6859feef8384
Jay Pipes <jaypipes@gmail.com>
2018-11-10 10:11:31 -0500

    metadata: server side property-schema get
    Adds the basics for getting a single property-schema object, specifying
    the partition, object type, property key and version of the schema.
    
    Issue #23

--- pkg/metadata/storage/property.go
+++ pkg/metadata/storage/property.go
@@ -1,27 +1,75 @@
 package storage
 
 import (
+	"fmt"
+
 	etcd "github.com/coreos/etcd/clientv3"
 	etcd_namespace "github.com/coreos/etcd/clientv3/namespace"
+	"github.com/gogo/protobuf/proto"
 
 	"github.com/runmachine-io/runmachine/pkg/abstract"
 	"github.com/runmachine-io/runmachine/pkg/cursor"
+	"github.com/runmachine-io/runmachine/pkg/errors"
 	pb "github.com/runmachine-io/runmachine/proto"
 )
 
 const (
-	_KEY_PROPERTY_SCHEMA = "property_schemas/"
+	_PROPERTY_SCHEMAS_KEY = "property-schemas/"
 )
 
-func (s *Store) kvPropertySchemas() etcd.KV {
-	return etcd_namespace.NewKV(s.kv, _KEY_PROPERTY_SCHEMA)
+func (s *Store) kvPropertySchemas(
+	partition string,
+) etcd.KV {
+	// The $PROPERTY_SCHEMAS key namespace is a shortcut to:
+	// $ROOT/partitions/by-uuid/{partition_uuid}/property-schemas
+	return etcd_namespace.NewKV(
+		s.kvPartition(partition),
+		_PROPERTY_SCHEMAS_KEY,
+	)
+}
+
+func (s *Store) PropertySchemaGet(
+	partition string,
+	objType string,
+	propSchemaKey string,
+	version uint32,
+) (*pb.PropertySchema, error) {
+	kv := s.kvPropertySchemas(partition)
+	ctx, cancel := s.requestCtx()
+	defer cancel()
+	key := fmt.Sprintf("by-type/%s/%s/%d", objType, propSchemaKey, version)
+	gr, err := kv.Get(ctx, key, etcd.WithPrefix())
+	if err != nil {
+		s.log.ERR("error getting key %s: %v", key, err)
+		return nil, err
+	}
+	nKeys := len(gr.Kvs)
+	if nKeys == 0 {
+		return nil, errors.ErrNotFound
+	} else if nKeys > 1 {
+		return nil, errors.ErrMultipleRecords
+	}
+	var obj *pb.PropertySchema
+	if err = proto.Unmarshal(gr.Kvs[0].Value, obj); err != nil {
+		return nil, err
+	}
+
+	return obj, nil
 }
 
 func (s *Store) PropertySchemaList(
 	req *pb.PropertySchemaListRequest,
 ) (abstract.Cursor, error) {
-	kv := s.kvPropertySchemas()
+	partition := req.Session.Partition.Uuid
+	if req.Filters != nil {
+		if len(req.Filters.Partitions) > 0 {
+			// TODO(jaypipes): loop through all searched-for partitions
+			partition = req.Filters.Partitions[0].Uuid
+		}
+	}
+	kv := s.kvPropertySchemas(partition)
 	ctx, cancel := s.requestCtx()
+	defer cancel()
 	resp, err := kv.Get(
 		ctx,
 		"/",
@@ -30,7 +78,6 @@ func (s *Store) PropertySchemaList(
 		// separate utility
 		etcd.WithSort(etcd.SortByKey, etcd.SortAscend),
 	)
-	cancel()
 	if err != nil {
 		s.log.ERR("error listing property schemas: %v", err)
 		return nil, err

1cba5a664dcb1c72c4766fb710ef00afefb341b1
Jay Pipes <jaypipes@gmail.com>
2018-11-14 21:14:25 -0500

    add `runm bootstrap` CLI command
    Adds the client side of the bootstrap process, which creates a new
    partition, supplying the one-time-use bootstrap token:
    
    ```
    [jaypipes@uberbox runmachine]$ ./scripts/dump-etcd.sh
    gsr/services/runmachine-metadata/172.17.0.3:10000
    runm-test-metadata/runm-metadata//
    runm-test-metadata/runm-metadata/bootstrap
    [jaypipes@uberbox runmachine]$ ./scripts/exec-runm-command.sh bootstrap bootstrapme part0
    1b561f953481442fb301a30f9c40994c
    [jaypipes@uberbox runmachine]$ ./scripts/dump-etcd.sh
    gsr/services/runmachine-metadata/172.17.0.3:10000
    runm-test-metadata/runm-metadata//
    runm-test-metadata/runm-metadata/partitions/by-name/part0
    runm-test-metadata/runm-metadata/partitions/by-uuid/1b561f953481442fb301a30f9c40994c
    [jaypipes@uberbox runmachine]$ ./scripts/exec-runm-command.sh bootstrap bootstrapme part0
    Error: Bootstrap failed.
    ```
    
    Note the bootstrap token is removed after successfully creating the
    partition, and attempting to bootstrap again fails afterwards.
    
    Issue #30

--- pkg/metadata/bootstrap.go
+++ pkg/metadata/bootstrap.go
@@ -5,6 +5,19 @@ import (
 
 	"github.com/google/uuid"
 	pb "github.com/runmachine-io/runmachine/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	ErrBootstrapTokenRequired = status.Errorf(
+		codes.FailedPrecondition,
+		"bootstrap token is required.",
+	)
+	ErrPartitionNameRequired = status.Errorf(
+		codes.FailedPrecondition,
+		"partition name is required.",
+	)
 )
 
 func (s *Server) Bootstrap(
@@ -12,14 +25,20 @@ func (s *Server) Bootstrap(
 	req *pb.BootstrapRequest,
 ) (*pb.BootstrapResponse, error) {
 	token := req.BootstrapToken
+	if token == "" {
+		return nil, ErrBootstrapTokenRequired
+	}
 	partName := req.PartitionName
+	if partName == "" {
+		return nil, ErrPartitionNameRequired
+	}
+
 	var partUuid string
 	if req.PartitionUuid == nil {
 		partUuid = uuid.New().String()
 	} else {
 		partUuid = req.PartitionUuid.Value
 	}
-
 	partUuid = normalizeUuid(partUuid)
 
 	if err := s.store.Bootstrap(token, partName, partUuid); err != nil {

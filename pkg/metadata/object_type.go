75954fb29a36ad81df212338dc3bb03117bfc436
Jay Pipes <jaypipes@gmail.com>
2018-11-18 11:38:29 -0500

    server-side implementation of ObjectTypeGet
    Adds the gRPC API server-side implementation of the ObjectTypeGet API
    method along with the storage layer implementation to return a single
    object type protobuffer message.
    
    Issue #38

--- pkg/metadata/object_type.go
+++ pkg/metadata/object_type.go
@@ -3,14 +3,42 @@ package metadata
 import (
 	"context"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/runmachine-io/runmachine/pkg/errors"
 	pb "github.com/runmachine-io/runmachine/proto"
+)
+
+var (
+	ErrCodeRequired = status.Errorf(
+		codes.FailedPrecondition,
+		"A code to search for is required.",
+	)
 )
 
 func (s *Server) ObjectTypeGet(
 	ctx context.Context,
 	req *pb.ObjectTypeGetRequest,
 ) (*pb.ObjectType, error) {
-	return nil, nil
+	if req.Code == "" {
+		return nil, ErrCodeRequired
+	}
+	obj, err := s.store.ObjectTypeGet(req.Code)
+	if err != nil {
+		if err == errors.ErrNotFound {
+			return nil, ErrNotFound
+		}
+		// We don't want to expose internal errors to the user, so just return
+		// an unknown error after logging it.
+		s.log.ERR(
+			"failed to retrieve object type of %s: %s",
+			req.Code,
+			err,
+		)
+		return nil, ErrUnknown
+	}
+	return obj, nil
 }
 
 func (s *Server) ObjectTypeList(

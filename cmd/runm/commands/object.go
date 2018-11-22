6371ea19e80a4ff69ef2d97ad9e70f0981765c78
Jay Pipes <jaypipes@gmail.com>
2018-11-22 05:15:25 -0500

    add `runm object list` CLI command
    Adds the `runm object list` command with filter options.
    
    Issue #43

---
+++ cmd/runm/commands/object.go
@@ -0,0 +1,14 @@
+package commands
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var objectCommand = &cobra.Command{
+	Use:   "object",
+	Short: "Manipulate object information",
+}
+
+func init() {
+	objectCommand.AddCommand(objectListCommand)
+}

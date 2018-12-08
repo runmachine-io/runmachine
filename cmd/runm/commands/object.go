3aed39d19882bd3c9882b5d3a92f6d3fe946d1e8
Jay Pipes <jaypipes@gmail.com>
2018-12-08 14:20:31 -0500

    output full object info after object create
    `runm object create` now prints out the entire Object upon receiving a
    successful result:
    
    ```
    [jaypipes@uberbox runmachine]$ ./scripts/exec-runm-command.sh object create -f tests/data/objects/runm.image.yaml
    Partition:   bc47cfc8ac964399ac4165618645af65
    Type:        runm.image
    UUID:        43f58536a3a54bcf968c7ea849b4a871
    Name:        db-server
    Project:     proj0
    Properties:
       architecture=x86_64
       os=linux
    Tags:       latest,active
    ```
    
    Moves printing functionality into a common printObject() function in
    cmd/runm/commands/object.go.
    
    Issue #43

--- cmd/runm/commands/object.go
+++ cmd/runm/commands/object.go
@@ -1,6 +1,10 @@
 package commands
 
 import (
+	"fmt"
+	"strings"
+
+	pb "github.com/runmachine-io/runmachine/proto"
 	"github.com/spf13/cobra"
 )
 
@@ -14,4 +18,24 @@ func init() {
 	objectCommand.AddCommand(objectGetCommand)
 	objectCommand.AddCommand(objectCreateCommand)
 	objectCommand.AddCommand(objectDeleteCommand)
+}
+
+func printObject(obj *pb.Object) {
+	fmt.Printf("Partition:   %s\n", obj.Partition)
+	fmt.Printf("Type:        %s\n", obj.Type)
+	fmt.Printf("UUID:        %s\n", obj.Uuid)
+	fmt.Printf("Name:        %s\n", obj.Name)
+	if obj.Project != "" {
+		fmt.Printf("Project:     %s\n", obj.Project)
+	}
+	if obj.Properties != nil {
+		fmt.Printf("Properties:\n")
+		for _, prop := range obj.Properties {
+			fmt.Printf("   %s=%s\n", prop.Key, prop.Value)
+		}
+	}
+	if obj.Tags != nil {
+		tags := strings.Join(obj.Tags, ",")
+		fmt.Printf("Tags:       %s\n", tags)
+	}
 }
--- cmd/runm/commands/object_get.go
+++ cmd/runm/commands/object_get.go
@@ -3,7 +3,6 @@ package commands
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/spf13/cobra"
 	"golang.org/x/net/context"
@@ -74,21 +73,5 @@ func objectGet(cmd *cobra.Command, args []string) {
 		},
 	)
 	exitIfError(err)
-	fmt.Printf("Partition:   %s\n", obj.Partition)
-	fmt.Printf("Type:        %s\n", obj.Type)
-	fmt.Printf("UUID:        %s\n", obj.Uuid)
-	fmt.Printf("Name:        %s\n", obj.Name)
-	if obj.Project != "" {
-		fmt.Printf("Project:     %s\n", obj.Project)
-	}
-	if obj.Properties != nil {
-		fmt.Printf("Properties:\n")
-		for _, prop := range obj.Properties {
-			fmt.Printf("   %s=%s\n", prop.Key, prop.Value)
-		}
-	}
-	if obj.Tags != nil {
-		tags := strings.Join(obj.Tags, ",")
-		fmt.Printf("Tags:       %s\n", tags)
-	}
+	printObject(obj)
 }

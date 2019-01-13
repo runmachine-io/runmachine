c0a7cc08ff8babb881182320dca9a2b2da4915fd
Jay Pipes <jaypipes@gmail.com>
2019-01-13 10:55:17 -0500

    add script to populate test fixtures
    Adds a simple script to populate a set of test fixtures for providers
    and partitions that are used in ad-hoc testing.

---
+++ scripts/populate.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+DEBUG=${DEBUG:-0}
+VERBOSE=${VERBOSE:-0}
+VERSION=$(git describe --tags --always --dirty)
+ROOT_DIR=$(cd $(dirname "$0")/.. && pwd)
+SCRIPTS_DIR=$ROOT_DIR/scripts
+LIB_DIR=$SCRIPTS_DIR/lib
+FIXTURES_DIR=$ROOT_DIR/tests/data/objects
+
+source $LIB_DIR/common
+
+check_is_installed docker
+
+source $LIB_DIR/container
+source $LIB_DIR/etcd
+source $LIB_DIR/mysql
+
+if debug_enabled; then
+    set -o xtrace
+fi
+
+for f in $FIXTURES_DIR/partitions/*; do
+    part_id=$( basename "$f")
+    $SCRIPTS_DIR/runm.sh partition create -f tests/data/objects/partitions/$part_id
+done
+
+for f in $FIXTURES_DIR/providers/*; do
+    prov_id=$( basename "$f")
+    $SCRIPTS_DIR/runm.sh provider create -f tests/data/objects/providers/$prov_id
+done

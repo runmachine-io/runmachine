213cbd6dc7bba9047f3e2827bce2da3cab0bca62
Jay Pipes <jaypipes@gmail.com>
2019-01-18 14:25:52 -0500

    Create provider definition on populate script

--- scripts/populate.sh
+++ scripts/populate.sh
@@ -6,7 +6,7 @@ VERSION=$(git describe --tags --always --dirty)
 ROOT_DIR=$(cd $(dirname "$0")/.. && pwd)
 SCRIPTS_DIR=$ROOT_DIR/scripts
 LIB_DIR=$SCRIPTS_DIR/lib
-FIXTURES_DIR=$ROOT_DIR/tests/data/objects
+FIXTURES_DIR=$ROOT_DIR/tests/data/
 
 source $LIB_DIR/common
 
@@ -16,13 +16,16 @@ fi
 
 source $SCRIPTS_DIR/service-up.sh
 
-for f in $FIXTURES_DIR/partitions/*; do
+for f in $FIXTURES_DIR/objects/partitions/*; do
     part_id=$( basename "$f")
     echo -n "creating partition '$part_id' ... "
     $SCRIPTS_DIR/runm.sh partition create -f tests/data/objects/partitions/$part_id
 done
 
-for f in $FIXTURES_DIR/providers/*; do
+echo -n "creating provider definition  ... "
+$SCRIPTS_DIR/runm.sh provider definition set -f tests/data/definitions/runm.provider.yaml
+
+for f in $FIXTURES_DIR/objects/providers/*; do
     prov_id=$( basename "$f")
     echo -n "creating provider '$prov_id' ... "
     $SCRIPTS_DIR/runm.sh provider create -f tests/data/objects/providers/$prov_id
